996912692623a71888c332c329b8fa3acccc73c3
joelvalenti <valentijoel18@gmail.com>
2020-03-04 16:34:20 -0300

    #9 Method minium and its test developed

--- src/test/java/calculator/OtherOperationsTest.java
+++ src/test/java/calculator/OtherOperationsTest.java
@@ -1,6 +1,8 @@
 package calculator;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.Assert.assertEquals;
 
@@ -15,7 +17,7 @@ public class OtherOperationsTest {
     Calculator calculator;
 
     @BeforeEach
-    public void before(){
+    public void before() {
         this.calculator = new Calculator();
     }
 
@@ -23,12 +25,13 @@ public class OtherOperationsTest {
     public void testMultiply() throws Exception {
 
         int expected = 5 * 5;
-        int result = this.calculator.multiply(5,5);
+        int result = this.calculator.multiply(5, 5);
         //traza
-        System.out.println("Expected: "+expected+ " - Result: "+result);
-        assertEquals(expected,result);
+        System.out.println("Expected: " + expected + " - Result: " + result);
+        assertEquals(expected, result);
     }
-@Test
+
+    @Test
     public void testModule() {
         int expected = 43 % 10;
         int result = this.calculator.module(43, 10);
@@ -39,4 +42,15 @@ public class OtherOperationsTest {
     public void testModuleDivByZero() {
         assertThrows(ArithmeticException.class, () -> this.calculator.module(10, 0), "Divide by zero should throw");
     }
+
+    @Test
+    public void testMinium() {
+        int operator1 = 5;
+        int operator2 = 7;
+        int expected = 5;
+        float result = this.calculator.minimum(operator1, operator2);
+        System.out.println("Expected: " + expected + ". Result: " + result);
+        assertEquals(expected, result, 0.0f);
+    }
+
 }
