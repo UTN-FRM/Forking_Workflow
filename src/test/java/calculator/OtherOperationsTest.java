1a2d49f8f7690553fad394a5d20978925be60ef8
Pablo García <pablonic.gar@gmail.com>
2020-03-03 21:57:06 -0300

    Se agrega el modulo multiply del issue #4 y su respectivo test

--- src/test/java/calculator/OtherOperationsTest.java
+++ src/test/java/calculator/OtherOperationsTest.java
@@ -1,4 +1,25 @@
 package calculator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertEquals;
 
 public class OtherOperationsTest {
+
+    Calculator calculator;
+
+    @BeforeEach
+    public void before(){
+        this.calculator = new Calculator();
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+
+        int expected = 5 * 5;
+        int result = this.calculator.multiply(5,5);
+        //traza
+        System.out.println("Expected: "+expected+ " - Result: "+result);
+        assertEquals(expected,result);
+    }
 }
